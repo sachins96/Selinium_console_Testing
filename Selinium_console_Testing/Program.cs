1e301d53e8b2240ac76750227f4f5c800f6dc9a5
sachins96 <sachinshelke96@gmail.com>
2020-05-18 11:15:54 +0530

    Saved Data

--- Selinium_console_Testing/Program.cs
+++ Selinium_console_Testing/Program.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Selinium_console_Testing
@@ -10,6 +13,25 @@ namespace Selinium_console_Testing
     {
         static void Main(string[] args)
         {
+            Console.Write("test case started ");
+            //create the reference for the browser  
+            IWebDriver driver = new ChromeDriver();
+            // navigate to URL  
+            driver.Navigate().GoToUrl("https://www.google.com/");
+            Thread.Sleep(2000);
+            // identify the Google search text box  
+            IWebElement ele = driver.FindElement(By.Name("q"));
+            //enter the value in the google search text box  
+            ele.SendKeys("ideas smart space");
+            Thread.Sleep(2000);
+            //identify the google search button  
+            IWebElement ele1 = driver.FindElement(By.Name("btnK"));
+            // click on the Google search button  
+            ele1.Click();
+            Thread.Sleep(3000);
+            //close the browser  
+            driver.Close();
+            Console.Write("test case ended ");
         }
     }
 }
